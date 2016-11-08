973a000c60e9f919a0c1909ef9d8494ce3f227cc
marcos <marcos@red-7.com.mx>
2016-11-08 18:11:39 -0500

    Creacion de Proyecto Index y Conexion BD

---
+++ conexionBD.php
@@ -0,0 +1,10 @@
+<?php
+		// sass --watch css/sass/styleEAT.sass:css/styleEAT.css
+		
+		$datosConexionBD[0] = 'mysql.red-7.com.mx'; // Colocamos el servidor donde está MySql
+		$datosConexionBD[1] = 'admin'; // Colocamos el usuario de Mysql
+		$datosConexionBD[2] = '7R@server'; // Colocamos la contraseña del usuario de Mysql
+		$datosConexionBD[3] = 'nucleoSales'; // Colocamos el nombre de la BD	
+
+		#BASE DE DATOS DE DIRECCIONES DE MÉXICO		BASE DE DATOS DE DIRECCIONES DE MÉXICO		BASE DE DATOS DE DIRECCIONES DE MÉXICO
+?>
